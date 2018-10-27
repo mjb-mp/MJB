851075b248a0e1197c713b19cd3086967a2c5586
mjb-mp <musicjukeboxsystem@gmail.com>
2018-10-27 10:07:43 +0530

    akhilesh

---
+++ MusicJukeBoxSystem/MJBException/MJBException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MJBException
+{
+    public class MJBException:ApplicationException
+    {
+        public MJBException() : base() { }
+
+        public MJBException(string message) : base(message) { }
+
+        public MJBException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
