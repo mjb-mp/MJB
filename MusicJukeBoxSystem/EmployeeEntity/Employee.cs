307c3db90a7b5e27fd2e9d9ad7ecf07ca445ee0f
mjb-mp <musicjukeboxsystem@gmail.com>
2018-10-27 12:20:28 +0530

    sam

---
+++ MusicJukeBoxSystem/EmployeeEntity/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeEntity
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmpName { get; set; }
+        public string Address { get; set; }
+        public DateTime DOB { get; set; }
+        public string City { get; set; }
+        public string Password { get; set; }
+        public string MobileNo { get; set; }
+    }
+}
