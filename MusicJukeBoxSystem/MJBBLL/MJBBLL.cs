76f26f7e1f865bbf774159dbab47e7bd4d332442
mjb-mp <musicjukeboxsystem@gmail.com>
2018-10-27 12:02:49 +0530

    Ak

---
+++ MusicJukeBoxSystem/MJBBLL/MJBBLL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomerEntity;
+namespace MJBBLL
+{
+    public class MJBBLL
+    {
+        //private static bool ValidateCustomer(Customer customer)
+        //{
+        //    StringBuilder sb = new StringBuilder();
+        //    bool validCustomer = true;
+
+
+        //}
+    }
+}
